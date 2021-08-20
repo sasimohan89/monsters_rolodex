134974e94d22eb53a8af2d15d5c3d25fffa41c69
sasimohan89 <sm9474699@gmail.com>
2021-08-20 11:52:04 -0400

    added images and card containers

--- src/App.js
+++ src/App.js
@@ -21,11 +21,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name='Sass'>
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name}</h1>
-        ))}
-        </CardList>
+        <CardList monsters={this.state.monsters}>
+          </CardList>
         
       </div>
     );
